dc2468fe38a84406aa9dfffa382909c968b189f4
maylee2016 <mayperez2010@gmail.com>
2019-07-22 18:54:25 -0400

    funciones faltantes

--- base/funciones/conta.ft_int_comprobante_sel.sql
+++ base/funciones/conta.ft_int_comprobante_sel.sql
@@ -844,8 +844,4 @@ LANGUAGE 'plpgsql'
 VOLATILE
 CALLED ON NULL INPUT
 SECURITY INVOKER
-PARALLEL UNSAFE
 COST 100;
-
-ALTER FUNCTION conta.ft_int_comprobante_sel (p_administrador integer, p_id_usuario integer, p_tabla varchar, p_transaccion varchar)
-  OWNER TO postgres;
