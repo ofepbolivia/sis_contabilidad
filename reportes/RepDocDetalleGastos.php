8fa1092e288b02d193ac518fa93f53d7e02b6aac
maylee2016 <mayperez2010@gmail.com>
2020-08-24 14:22:19 -0400

    modificacion reporte detalle de gastos columnas

--- reportes/RepDocDetalleGastos.php
+++ reportes/RepDocDetalleGastos.php
@@ -86,38 +86,35 @@ class RepDocDetalleGastos
         //titulos
 
         $this->docexcel->getActiveSheet()->setCellValueByColumnAndRow(0, 2, 'DETALLE DE GASTOS');
-        $this->docexcel->getActiveSheet()->getStyle('A2:W2')->applyFromArray($styleTitulos1);
-        $this->docexcel->getActiveSheet()->mergeCells('A2:W2');
+        $this->docexcel->getActiveSheet()->getStyle('A2:T2')->applyFromArray($styleTitulos1);
+        $this->docexcel->getActiveSheet()->mergeCells('A2:T2');
         $this->docexcel->getActiveSheet()->setCellValueByColumnAndRow(0, 3, 'Del: ' . $this->objParam->getParametro('fecha_ini') . '   Al: ' . $this->objParam->getParametro('fecha_fin'));
-        $this->docexcel->getActiveSheet()->getStyle('A3:W3')->applyFromArray($styleTitulos3);
-        $this->docexcel->getActiveSheet()->mergeCells('A3:W3');
+        $this->docexcel->getActiveSheet()->getStyle('A3:T3')->applyFromArray($styleTitulos3);
+        $this->docexcel->getActiveSheet()->mergeCells('A3:T3');
 
         //*************************************FIN TITULO*****************************************
 
 
         $this->docexcel->getActiveSheet()->getColumnDimension('A')->setWidth(25);
-        $this->docexcel->getActiveSheet()->getColumnDimension('B')->setWidth(20);
-        $this->docexcel->getActiveSheet()->getColumnDimension('C')->setWidth(40);
-        $this->docexcel->getActiveSheet()->getColumnDimension('D')->setWidth(25);
-        $this->docexcel->getActiveSheet()->getColumnDimension('E')->setWidth(20);
-        $this->docexcel->getActiveSheet()->getColumnDimension('F')->setWidth(10);
+        $this->docexcel->getActiveSheet()->getColumnDimension('B')->setWidth(15);
+        $this->docexcel->getActiveSheet()->getColumnDimension('C')->setWidth(18);
+        $this->docexcel->getActiveSheet()->getColumnDimension('D')->setWidth(50);
+        $this->docexcel->getActiveSheet()->getColumnDimension('E')->setWidth(25);
+        $this->docexcel->getActiveSheet()->getColumnDimension('F')->setWidth(15);
         $this->docexcel->getActiveSheet()->getColumnDimension('G')->setWidth(50);
-        $this->docexcel->getActiveSheet()->getColumnDimension('H')->setWidth(25);
-        $this->docexcel->getActiveSheet()->getColumnDimension('I')->setWidth(25);
+        $this->docexcel->getActiveSheet()->getColumnDimension('H')->setWidth(50);
+        $this->docexcel->getActiveSheet()->getColumnDimension('I')->setWidth(50);
         $this->docexcel->getActiveSheet()->getColumnDimension('J')->setWidth(40);
-        $this->docexcel->getActiveSheet()->getColumnDimension('K')->setWidth(80);
-        $this->docexcel->getActiveSheet()->getColumnDimension('L')->setWidth(40);
+        $this->docexcel->getActiveSheet()->getColumnDimension('K')->setWidth(50);
+        $this->docexcel->getActiveSheet()->getColumnDimension('L')->setWidth(25);
         $this->docexcel->getActiveSheet()->getColumnDimension('M')->setWidth(25);
-        $this->docexcel->getActiveSheet()->getColumnDimension('N')->setWidth(25);
-        $this->docexcel->getActiveSheet()->getColumnDimension('O')->setWidth(25);
-        $this->docexcel->getActiveSheet()->getColumnDimension('P')->setWidth(40);
-        $this->docexcel->getActiveSheet()->getColumnDimension('Q')->setWidth(60);
-        $this->docexcel->getActiveSheet()->getColumnDimension('R')->setWidth(20);
-        $this->docexcel->getActiveSheet()->getColumnDimension('S')->setWidth(15);
-        $this->docexcel->getActiveSheet()->getColumnDimension('T')->setWidth(15);
-        $this->docexcel->getActiveSheet()->getColumnDimension('U')->setWidth(15);
-        $this->docexcel->getActiveSheet()->getColumnDimension('V')->setWidth(40);
-        $this->docexcel->getActiveSheet()->getColumnDimension('W')->setWidth(50);
+        $this->docexcel->getActiveSheet()->getColumnDimension('N')->setWidth(35);
+        $this->docexcel->getActiveSheet()->getColumnDimension('O')->setWidth(60);
+        $this->docexcel->getActiveSheet()->getColumnDimension('P')->setWidth(20);
+        $this->docexcel->getActiveSheet()->getColumnDimension('Q')->setWidth(15);
+        $this->docexcel->getActiveSheet()->getColumnDimension('R')->setWidth(15);
+        $this->docexcel->getActiveSheet()->getColumnDimension('S')->setWidth(60);
+        $this->docexcel->getActiveSheet()->getColumnDimension('T')->setWidth(50);
 
 
         $styleTitulos = array(
@@ -166,37 +163,34 @@ class RepDocDetalleGastos
                     'style' => PHPExcel_Style_Border::BORDER_THIN
                 )
             ));
-        $this->docexcel->getActiveSheet()->getStyle('A4:W4')->getAlignment()->setWrapText(true);
+        $this->docexcel->getActiveSheet()->getStyle('A4:T4')->getAlignment()->setWrapText(true);
 
-        $this->docexcel->getActiveSheet()->getStyle('A4:W4')->applyFromArray($styleTitulos);
+        $this->docexcel->getActiveSheet()->getStyle('A4:T4')->applyFromArray($styleTitulos);
 
 
-       // $this->docexcel->getActiveSheet()->getStyle("S:U")->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat :: FORMAT_NUMBER_COMMA_SEPARATED1);
+        $this->docexcel->getActiveSheet()->getStyle("Q:R")->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat :: FORMAT_NUMBER_COMMA_SEPARATED1);
 
         //*************************************Cabecera*****************************************
-        $this->docexcel->getActiveSheet()->setCellValue('A4','NRO OP');
-        $this->docexcel->getActiveSheet()->setCellValue('B4','NRO REGISTRO');
-        $this->docexcel->getActiveSheet()->setCellValue('C4','NRO OP - NRO REGISTRO');
-        $this->docexcel->getActiveSheet()->setCellValue('D4','FECHA CBTE');
-        $this->docexcel->getActiveSheet()->setCellValue('E4','TIPO DE CAMBIO');
-        $this->docexcel->getActiveSheet()->setCellValue('F4','');
-        $this->docexcel->getActiveSheet()->setCellValue('G4','PROVEEDOR');
-        $this->docexcel->getActiveSheet()->setCellValue('H4','TIPO DE DOCUMENTO');
-        $this->docexcel->getActiveSheet()->setCellValue('I4','NRO FACTURA');
-        $this->docexcel->getActiveSheet()->setCellValue('J4','CONCEPTO DE GASTO');
-        $this->docexcel->getActiveSheet()->setCellValue('K4','DETALLE');
-        $this->docexcel->getActiveSheet()->setCellValue('L4','ORDEN DE TRABAJO');
-        $this->docexcel->getActiveSheet()->setCellValue('M4','PARTIDA PRESUPUESTARIA');
-        $this->docexcel->getActiveSheet()->setCellValue('N4','PERIODO DEL GASTO - INICIO');
-        $this->docexcel->getActiveSheet()->setCellValue('O4','PERIODO DEL GASTO - FIN');
-        $this->docexcel->getActiveSheet()->setCellValue('P4','DETALLE - PERIODO DEL GASTO');
-        $this->docexcel->getActiveSheet()->setCellValue('Q4','DETALLE DE GASTOS');
-        $this->docexcel->getActiveSheet()->setCellValue('R4','NRO CHEQUE');
-        $this->docexcel->getActiveSheet()->setCellValue('S4','IMPORTE USD');
-        $this->docexcel->getActiveSheet()->setCellValue('T4','IMPORTE BOL');
-        $this->docexcel->getActiveSheet()->setCellValue('U4','IMPORTE ARG');
-        $this->docexcel->getActiveSheet()->setCellValue('V4','CUENTA');
-        $this->docexcel->getActiveSheet()->setCellValue('W4','GLOSA');
+        $this->docexcel->getActiveSheet()->setCellValue('A4','NRO CBTE');
+        $this->docexcel->getActiveSheet()->setCellValue('B4','FECHA CBTE');
+        $this->docexcel->getActiveSheet()->setCellValue('C4','TIPO DE CAMBIO');
+        $this->docexcel->getActiveSheet()->setCellValue('D4','PROVEEDOR');
+        $this->docexcel->getActiveSheet()->setCellValue('E4','TIPO DE DOCUMENTO');
+        $this->docexcel->getActiveSheet()->setCellValue('F4','NRO FACTURA');
+        $this->docexcel->getActiveSheet()->setCellValue('G4','CONCEPTO DE GASTO');
+        $this->docexcel->getActiveSheet()->setCellValue('H4','DETALLE');
+        $this->docexcel->getActiveSheet()->setCellValue('I4','CENTRO DE COSTO');
+        $this->docexcel->getActiveSheet()->setCellValue('J4','PARTIDA PRESUPUESTARIA');
+        $this->docexcel->getActiveSheet()->setCellValue('K4','ORDEN DE TRABAJO');
+        $this->docexcel->getActiveSheet()->setCellValue('L4','PERIODO DEL GASTO - INICIO');
+        $this->docexcel->getActiveSheet()->setCellValue('M4','PERIODO DEL GASTO - FIN');
+        $this->docexcel->getActiveSheet()->setCellValue('N4','DETALLE - PERIODO DEL GASTO');
+        $this->docexcel->getActiveSheet()->setCellValue('O4','DETALLE DE GASTOS');
+        $this->docexcel->getActiveSheet()->setCellValue('P4','NRO CHEQUE');
+        $this->docexcel->getActiveSheet()->setCellValue('Q4','IMPORTE $');
+        $this->docexcel->getActiveSheet()->setCellValue('R4','IMPORTE');
+        $this->docexcel->getActiveSheet()->setCellValue('S4','CUENTA BANCARIA');
+        $this->docexcel->getActiveSheet()->setCellValue('T4','GLOSA');
 
 
     }
