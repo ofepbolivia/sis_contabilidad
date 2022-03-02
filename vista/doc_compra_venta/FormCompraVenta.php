cd7482a8ad4c7e1f806853a0d48883454bde60e9
bradt123 <bradtbrey1234@gmail.com>
2022-03-02 12:38:53 -0400

    habilitacion nro factura plantilla factura no declarada

--- vista/doc_compra_venta/FormCompraVenta.php
+++ vista/doc_compra_venta/FormCompraVenta.php
@@ -1914,7 +1914,7 @@ header("content-type: text/javascript; charset=UTF-8");
                     this.Cmp.nro_dui.allowBlank = false;
                     this.mostrarComponente(this.Cmp.nro_dui);
                     this.Cmp.nro_documento.setValue(0);
-                    this.Cmp.nro_documento.setReadOnly(true);
+                    // this.Cmp.nro_documento.setReadOnly(true);
 
                 }
                 else {
